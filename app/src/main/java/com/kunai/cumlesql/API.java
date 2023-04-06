5da099494513dc38ee66f6e21b0d85526d7da5cb
furkanturgut68 <furkanmehmeturgut@gmail.com>
2023-04-06 15:47:01 +0300

    delete my api

--- app/src/main/java/com/kunai/cumlesql/API.java
+++ app/src/main/java/com/kunai/cumlesql/API.java
@@ -2,5 +2,5 @@ package com.kunai.cumlesql;
 
 public class API {
     public static String API_URL = "https://api.openai.com/v1/completions";
-    public static String API = "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";
+    public static String API = "";
 }
